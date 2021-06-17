e246fafebc7021705cc5953767b066cf70db584b
Proshenzit <37993972+Proshenzit@users.noreply.github.com>
2021-06-17 02:07:24 -0700

    Add files via upload

---
+++ Harry/class4.js
@@ -0,0 +1,82 @@
+// primative data type 
+
+//string
+
+let   x="hello";
+console.log("my  String is "+ x);
+console.log("my data type is "+(typeof x));
+
+
+// Numbers 
+
+let marks = 34;
+
+console.log("Data type  is "+ (typeof marks));
+
+// boolean
+
+let isDriver = true;
+
+console.log("Data type  is "+ (typeof isDriver));
+
+//Null
+
+let nullvar=null;
+
+console.log("Data type  is "+ (typeof nullvar));
+
+//undefined
+
+let undef = undefined;
+
+console.log("Data type  is "+ (typeof undefined));
+
+// Refernce data type
+
+//Array
+
+ let myarr = [1,,2,3,4,5,6,"string"];
+
+console.log("Data type  is "+ (typeof myarr));
+
+// objects Literals
+
+let  stmarks={
+
+
+    harry:89,
+    roy:112,
+    zit:343,
+    zty:235
+}
+
+console.log( typeof stmarks)
+
+
+
+function findName() {
+    
+}
+
+console.log(typeof findName);
+
+let date = new Date();
+
+console.log(typeof date);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
